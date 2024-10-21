b75d5d58360a3d2217d5a7ac11d12c744fce69c5
FolashadeOmoloja <omolojashade@gmail.com>
2024-10-21 03:50:12 +0100

    admin dashboard

--- components/ManageTalents/TalentTables.tsx
+++ components/ManageTalents/TalentTables.tsx
@@ -5,30 +5,92 @@ import { userObject } from "@/utilities/typeDefs";
 import { Loader2 } from "lucide-react";
 import MainTable from "../Elements/Table/MainTable";
 import { useGetAllTalents } from "@/hooks/admin-analytics-hook";
+import { useRouter } from "next/navigation";
+import { FaArrowLeft } from "react-icons/fa6";
+import { useState } from "react";
 
+type IsActiveState = {
+  [key: number]: boolean;
+};
 const TalentTable = () => {
-  const filterArr = ["Frack Talents Pool"];
+  const filterArr = [
+    "Registered Talent",
+    "Frack Talents Pool",
+    "Waitlist Talents",
+  ];
+  const [active, setActive] = useState<IsActiveState>({ 0: true });
+  const [changeTable, setChangeTable] = useState(0);
   const { talents, loading } = useGetAllTalents();
+  const router = useRouter();
+
+  const filterTalents = (accountStatus: string) => {
+    return talents.filter((talent) =>
+      //@ts-ignore
+      talent.accountStatus?.toLowerCase().includes(accountStatus.toLowerCase())
+    );
+  };
+  const shortList = changeTable === 1 ? filterTalents("shortlist") : [];
+  const waitList = changeTable === 2 ? filterTalents("waitlist") : [];
+
+  const activeFunc = (idx: number) => {
+    const newState: IsActiveState = {};
+    filterArr.forEach((_, i) => (newState[i] = i === idx));
+    setActive(newState);
+    setChangeTable(idx);
+  };
 
   return (
     <section className="dashboard-container min-h-svh">
+      <div
+        onClick={() => router.push("/control-room")}
+        className="flex text-[#000080] gap-3 text-xl items-center font-bold mb-6 cursor-pointer"
+      >
+        <FaArrowLeft />
+        <span>Go back</span>
+      </div>
       <h2 className="text-2xl font-bold mb-1">Manage Frack's Talent Pool</h2>
       <div className="flex w-full text-[#626263] md:text-lg font-bold mt-16 border-b border-[#CCD2D9]">
         {filterArr.map((item, idx) => (
-          <span className={`tab active max-sm:h-[50px]`} key={idx}>
+          <span
+            className={`tab ${active[idx] ? "active" : ""} max-sm:h-[50px]`}
+            key={idx}
+            onClick={() => activeFunc(idx)}
+          >
             {item}
           </span>
         ))}
       </div>
-      {loading ? (
-        <Loader2 className=" h-14 w-14 animate-spin ml-10 mt-10 text-[#000080]" />
-      ) : talents.length === 0 ? (
-        <p className="mt-10 text-[#000040] italic text-2xl">
-          No data available at the moment.
-        </p>
-      ) : (
-        <MainTable<userObject> data={talents} columns={talentsColumn} />
-      )}
+      {changeTable === 0 ? (
+        loading ? (
+          <Loader2 className=" h-14 w-14 animate-spin ml-10 mt-10 text-[#000080]" />
+        ) : talents.length === 0 ? (
+          <p className="mt-10 text-[#000040] italic text-2xl">
+            No data available at the moment.
+          </p>
+        ) : (
+          <MainTable<userObject> data={talents} columns={talentsColumn} />
+        )
+      ) : changeTable === 1 ? (
+        loading ? (
+          <Loader2 className=" h-14 w-14 animate-spin ml-10 mt-10 text-[#000080]" />
+        ) : talents.length === 0 ? (
+          <p className="mt-10 text-[#000040] italic text-2xl">
+            No data available at the moment.
+          </p>
+        ) : (
+          <MainTable<userObject> data={shortList} columns={talentsColumn} />
+        )
+      ) : changeTable === 2 ? (
+        loading ? (
+          <Loader2 className=" h-14 w-14 animate-spin ml-10 mt-10 text-[#000080]" />
+        ) : talents.length === 0 ? (
+          <p className="mt-10 text-[#000040] italic text-2xl">
+            No data available at the moment.
+          </p>
+        ) : (
+          <MainTable<userObject> data={waitList} columns={talentsColumn} />
+        )
+      ) : null}
     </section>
   );
 };
